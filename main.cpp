1062e19ab4f5490a8c36c6240a2cf9c7813c362f
John Zhou <john.zhou81@myhunter.cuny.edu>
2022-02-14 16:42:38 -0500

    worked on task a

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 
 int main()
 {
-  std::cout << get_east_storage("") << "\n";
+  std::cout << "get_east_storage(\"01/01/2018\") returns " << get_east_storage("01/01/2018") << "\n";
+  std::cout << "get_east_storage(\"07/04/2018\") returns " << get_east_storage("07/04/2018") << "\n";
+  std::cout << "get_east_storage(\"12/10/2018\") returns " << get_east_storage("12/10/2018") << "\n";
   return 0;
 }
--- reservoir.cpp
+++ reservoir.cpp
@@ -6,14 +6,12 @@
 
 std::vector<std::string> tabSplit(std::string str)
 {
-
   int lastCutPoint = 0;
   std::vector<std::string> result = {};
   for (int i = 0; i < str.length()-1; i++)
   {
     std::string currString = str.substr(i,1);
     // std::cout << currString << std::endl;
-
     if (currString == "\t")
     {
       result.push_back(str.substr(lastCutPoint, i));
@@ -23,13 +21,11 @@ std::vector<std::string> tabSplit(std::string str)
       //std::cout << "compare method returned " << currString.compare("\t") << "\n";
     }
   }
-
   return result;
 }
 
 double get_east_storage(std::string date)
 {
-  std::vector<std::string> arr;
   // returns East Basin storage for given date
   std::ifstream file;
   file.open("Current_Reservoir_Levels.tsv");
@@ -38,27 +34,23 @@ double get_east_storage(std::string date)
     std::cerr << "File cannot be opened for reading.\n";
     exit(1);
   }
-
   // ignore first line by reading it first (first line is header)
   std::string header;
   std::getline(file, header);
-
   // now read line by line
   std::string currLn;
 
   while (std::getline(file, currLn)) // while you can still read
   {
     // split string line into an array
-
-      arr = tabSplit(currLn);
+      std::vector<std::string> arr = tabSplit(currLn);
+      if (arr[0] == date)
+      {
+        return std::stod(arr[1]);
+      }
       //std::cout << arr.size() << "\n";
-      std::cout << "output is " << arr[0] << "\n";
-      break;
-
-
-
-
+      //std::cout << "output is " << arr[0] << "\n";
   }
   file.close();
-  return std::stod(arr[1]);
+  return -1;
 }
